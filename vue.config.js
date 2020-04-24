146d7c432c8a84a690f4723fbfb609d4e9769e87
ifvv <ifvv@pm.me>
2020-04-24 13:31:34 +0800

    修改title

--- vue.config.js
+++ vue.config.js
@@ -40,7 +40,7 @@ module.exports = {
       filename: 'index.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'GONEList'
+      title: 'Cloud Drive'
     }
   }
 }
